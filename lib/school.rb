0e019503fc162b79fb5b95e87c1414c2909c0ef5
Shaina C <scailles23@gmail.com>
2020-12-31 04:03:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -24,8 +24,8 @@ class School
   end
   
   def sort
-    @roster.each do |grade, student|
-      @roster[grade].student.sort
+    @roster.each do |grade, name|
+      @roster[grade].name.sort
     end
   end
 end
